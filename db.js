f23c7fd0a10a083543f9d3e586d5f00d3476dd26
AnuwatRoopmoa <118360770+AnuwatRoopmoa@users.noreply.github.com>
2023-08-24 23:13:54 +0200

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ import mysql from 'mysql';
 
 // Create a connection pool
 export const pool = mysql.createPool({
-  host: 'sql303.byetcluster.com',
+  host: 'sql303.infinityfree.com',
   user: 'if0_34890453',
   password: 'UWG54PJC8G',
   database: 'if0_34890453_express'
