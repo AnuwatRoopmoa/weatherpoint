a070fe7dff5b1a831f27dd1884d5c26051334c44
AnuwatRoopmoa <118360770+AnuwatRoopmoa@users.noreply.github.com>
2023-08-24 23:04:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { app } from './app.js';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3306;
 
 app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
